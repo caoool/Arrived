a391a543e61e4b9fa1674d12fde855b30c157fd6
caoool <caoool.link@icloud.com>
2015-05-23 09:16:53 -0700

    nothing

--- Classes/VCs/ProfileVC.swift
+++ Classes/VCs/ProfileVC.swift
@@ -13,14 +13,17 @@ class ProfileVC: UIViewController {
     @IBOutlet weak var bg: UIImageView!
     @IBOutlet weak var portrait: UIImageView!
     
-    @IBOutlet weak var balanceImage: UIImageView!
-    @IBOutlet weak var balanceLabel: UILabel!
+    @IBOutlet weak var readyButton: UIButton!
     
-    @IBOutlet weak var tasksImage: UIImageView!
-    @IBOutlet weak var tasksLabel: UILabel!
-    
-    @IBOutlet weak var likesImage: UIImageView!
-    @IBOutlet weak var likesLabel: UILabel!
+    private var readyForJobs: Bool = true {
+        didSet {
+            if readyForJobs {
+                readyButton.setImage(UIImage(named: "status ok"), forState: .Normal)
+            } else {
+                readyButton.setImage(UIImage(named: "status no"), forState: .Normal)
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,32 +47,26 @@ class ProfileVC: UIViewController {
         bg.addSubview(blurView)
         
         // draw border of circle image
+        portrait.image = UIImage(named: "my portrait")
         portrait.layer.borderWidth = 5
         portrait.layer.masksToBounds = true
         portrait.layer.borderColor = UIColorFromHex(0xFFFFFF, alpha: 1).CGColor
         portrait.layer.cornerRadius = portrait.frame.height/2
         portrait.clipsToBounds = true
+        view.bringSubviewToFront(portrait)
     }
     
     func initScene() {
         
-        
-        portrait.image = UIImage(named: "my portrait")
-        
-        let balanceImageColored = balanceImage.image?.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
-        balanceImage.image = balanceImageColored
-        balanceImage.tintColor = UIColorFromHex(0x7474BF, alpha: 1)
-        
-        let tasksImageColored = tasksImage.image?.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
-        tasksImage.image = tasksImageColored
-        tasksImage.tintColor = UIColorFromHex(0xF09819, alpha: 1)
-        
-        let likesImageColored = likesImage.image?.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
-        likesImage.image = likesImageColored
-        likesImage.tintColor = UIColorFromHex(0xf857a6, alpha: 1)
-        
     }
     
+    @IBAction func changeStatus(sender: UIButton) {
+        if readyForJobs {
+            readyForJobs = false
+        } else {
+            readyForJobs = true
+        }
+    }
     
     
 }
