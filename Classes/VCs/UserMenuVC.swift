1d77f6316b408de083d4e8c095ea4a8501141e36
caoool <caoool.link@icloud.com>
2015-05-26 07:34:13 -0700

    Phone change added

--- Classes/VCs/UserMenuVC.swift
+++ Classes/VCs/UserMenuVC.swift
@@ -38,6 +38,11 @@ class UserMenuVC: UIViewController, UITextFieldDelegate {
     private var pinChanngeTextField3: UITextField!
     private var pinChanngeTextField4: UITextField!
     
+    private var phoneChanngeTextField1: UITextField!
+    private var phoneChanngeTextField2: UITextField!
+    private var phoneChanngeTextField3: UITextField!
+    private var phoneChanngeTextField4: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -95,7 +100,7 @@ class UserMenuVC: UIViewController, UITextFieldDelegate {
             textField.secureTextEntry = true
         }
         
-        alert.addAction(SimpleAlert.Action(title: "resend", style: .Default) { action in
+        alert.addAction(SimpleAlert.Action(title: "Resend ↻", style: .Default) { action in
 
             })
         
@@ -109,6 +114,86 @@ class UserMenuVC: UIViewController, UITextFieldDelegate {
         
         alert.addAction(SimpleAlert.Action(title: "⇠", style: .Cancel) { action in
 
+            })
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func changePhone(sender: UIButton) {
+        let alert = AlertController(title: "Phone Number", message: "We need to verify you identity in order to protect your account, please enter verification code sent to your phone number 😘", style: .Alert)
+        showChangePhoneAlert(alert)
+    }
+    
+    func showChangePhoneAlert(alert: SimpleAlert.Controller) {
+        
+        var verificationCode: Int?
+        var oldPhone: Int?
+        var newPhone: Int?
+        
+        alert.addTextFieldWithConfigurationHandler() { textField in
+            self.addDoneButtonOnKeyboard(textField)
+            self.phoneChanngeTextField1 = textField
+            textField.keyboardType = UIKeyboardType.NumberPad
+            textField.placeholder = "Verification code"
+        }
+        
+        alert.addTextFieldWithConfigurationHandler() { textField in
+            self.addDoneButtonOnKeyboard(textField)
+            self.phoneChanngeTextField2 = textField
+            textField.keyboardType = UIKeyboardType.NumberPad
+            textField.placeholder = "Old phone number"
+        }
+        
+        alert.addTextFieldWithConfigurationHandler() { textField in
+            self.addDoneButtonOnKeyboard(textField)
+            self.phoneChanngeTextField3 = textField
+            textField.keyboardType = UIKeyboardType.NumberPad
+            textField.placeholder = "New phone number"
+        }
+        
+        alert.addAction(SimpleAlert.Action(title: "Resend ↻", style: .Default) { action in
+            
+            })
+        
+        alert.addAction(SimpleAlert.Action(title: "Get Verification ⇢", style: .OK) { action in
+            
+            verificationCode = (alert.textFields[0] as UITextField).text.toInt()
+            oldPhone = (alert.textFields[1] as UITextField).text.toInt()
+            newPhone = (alert.textFields[2] as UITextField).text.toInt()
+            
+            let alert = AlertController(title: "Verify", message: "We need to verify you identity in order to protect your account, please enter verification code sent to your phone number 😘", style: .Alert)
+            self.showChangePhoneVerificationAlert(alert)
+            })
+        
+        alert.addAction(SimpleAlert.Action(title: "⇠", style: .Cancel) { action in
+            
+            })
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func showChangePhoneVerificationAlert(alert: SimpleAlert.Controller) {
+        
+        var verificationCode: Int?
+        
+        alert.addTextFieldWithConfigurationHandler() { textField in
+            self.addDoneButtonOnKeyboard(textField)
+            self.phoneChanngeTextField4 = textField
+            textField.keyboardType = UIKeyboardType.NumberPad
+            textField.placeholder = "Verification code"
+        }
+        
+        alert.addAction(SimpleAlert.Action(title: "resend ↻", style: .Default) { action in
+            
+            })
+        
+        alert.addAction(SimpleAlert.Action(title: "Confirm", style: .OK) { action in
+            verificationCode = (alert.textFields[0] as UITextField).text.toInt()
+            })
+        
+        alert.addAction(SimpleAlert.Action(title: "⇠", style: .Cancel) { action in
+            
             })
         
         self.presentViewController(alert, animated: true, completion: nil)
@@ -147,6 +232,14 @@ class UserMenuVC: UIViewController, UITextFieldDelegate {
             pinChanngeTextField3.resignFirstResponder()
         }  else if pinChanngeTextField4.isFirstResponder() {
             pinChanngeTextField4.resignFirstResponder()
+        } else if phoneChanngeTextField1.isFirstResponder() {
+            phoneChanngeTextField1.resignFirstResponder()
+        } else if phoneChanngeTextField2.isFirstResponder() {
+            phoneChanngeTextField2.resignFirstResponder()
+        } else if phoneChanngeTextField3.isFirstResponder() {
+            phoneChanngeTextField3.resignFirstResponder()
+        }  else if phoneChanngeTextField4.isFirstResponder() {
+            phoneChanngeTextField4.resignFirstResponder()
         }
     }
     
